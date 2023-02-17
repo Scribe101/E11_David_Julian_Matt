e4ecbe6fc4cb04fb3de17dc01fb3b0d6b8e61a41
Scribe101 <123216616+Scribe101@users.noreply.github.com>
2023-02-17 15:10:25 -0800

    Create week5_lab.py

---
+++ week5_lab.py
@@ -0,0 +1,33 @@
+import sys
+import random
+import time
+
+start_time = int(time.time())
+itime = start_time
+run_time = 10
+
+while itime < start_time + run_time:
+  print()
+  itime = time.time()
+  print("Time: "+str(itime))
+  print("Concentration Units (standard)")
+  print("---------------------------------------")
+  print(
+    "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+    % (random.random(), random.random(), random.random())
+  )
+  print("Concentration Units (environmental)")
+  print("---------------------------------------")
+  print(
+    "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+    % (random.random(), random.random(), random.random())
+  )
+  print("---------------------------------------")
+  print("Particles > 0.3um / 0.1L air:", random.random())
+  print("Particles > 0.5um / 0.1L air:", random.random())
+  print("Particles > 1.0um / 0.1L air:", random.random())
+  print("Particles > 2.5um / 0.1L air:", random.random())
+  print("Particles > 5.0um / 0.1L air:", random.random())
+  print("Particles > 10 um / 0.1L air:", random.random())
+  print("---------------------------------------")
+  time.sleep(1)
