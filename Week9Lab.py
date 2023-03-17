341cfcfcfe2668e5f7cd80314450eac1a36c519c
Scribe101 <123216616+Scribe101@users.noreply.github.com>
2023-03-17 15:52:45 -0700

    Update Week9Lab.py

--- Week9Lab.py
+++ Week9Lab.py
@@ -9,17 +9,24 @@ GPIO.setup(channel, GPIO.IN)
 startTime=int(time.time())
 iTime=startTime
 count = 0
-while iTime < (startTime + 12):
+list_of_times = []
+list_of_cpm = []
+while iTime < (startTime + 5):
  temp = GPIO.wait_for_edge(channel, GPIO.FALLING, timeout = 5000)
  if temp is None:
   print("Timeout")
  else:
   count = count + 1
-  print(str(datetime.datetime.now()))
+  #print(str(datetime.datetime.now()))
  #GPIO.wait_for_edge(channel, GPIO.FALLING)
  #counts = counts + 1
  #print(str(datetime.datetime.now()))
+ list_of_times.append(str(datetime.datetime.now()))
  #time.sleep(0.001)
  iTime = int(time.time())
+ if iTime > (start + 60):
+  list_of_cpm.append(count)
+  count = 0
+  start = int(time.time())
  
 print("Total Counts:" + str(count))
