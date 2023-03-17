c56d5a23df600d7b91dce4839926c8a61cc99ec2
Scribe101 <123216616+Scribe101@users.noreply.github.com>
2023-03-17 16:04:51 -0700

    Update Week9Lab.py

--- Week9Lab.py
+++ Week9Lab.py
@@ -19,22 +19,18 @@ count = 0
 list_of_times = []
 originalStart = int(time.time())
 
-while iTime < (originalStart + 13):
+while iTime < (originalStart + 140):
  temp = GPIO.wait_for_edge(channel, GPIO.FALLING, timeout = 1000)
  if temp is None:
   print("Timeout")
  else:
   count = count + 1
-  #print(str(datetime.datetime.now()))
- #GPIO.wait_for_edge(channel, GPIO.FALLING)
- #counts = counts + 1
- #print(str(datetime.datetime.now()))
  list_of_times.append(str(datetime.datetime.now()))
- #time.sleep(0.001)
  iTime = int(time.time())
- if iTime > (startTime + 3):
+ if iTime > (startTime + 60):
   data = [str(count),str(list_of_times)]
   writer.writerow(data)
+  print("Counts in the last minute: " + str(count))
   count = 0
   list_of_times = []
   startTime = int(time.time())
