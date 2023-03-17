82ae876f891266f46ab8b139148f98bc6e39a235
Scribe101 <123216616+Scribe101@users.noreply.github.com>
2023-03-17 15:26:40 -0700

    Update Week9Lab.py

--- Week9Lab.py
+++ Week9Lab.py
@@ -17,8 +17,7 @@ try:
  GPIO.setmode(GPIO.BCM)
  GPIO.setup(6, GPIO.IN)
  GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback)
-
-channel = GPIO.wait_for_edge(channel, GPIO.RISING, timeout=5000)
+ channel = GPIO.wait_for_edge(channel, GPIO.RISING, timeout=5000)
 
 
 
