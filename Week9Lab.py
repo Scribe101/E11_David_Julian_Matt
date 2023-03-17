a76a3706567479db345a7c009da8cc238aed9c8d
Scribe101 <123216616+Scribe101@users.noreply.github.com>
2023-03-17 15:18:35 -0700

    Update Week9Lab.py

--- Week9Lab.py
+++ Week9Lab.py
@@ -3,7 +3,8 @@
 import RPi.GPIO as GPIO
 import datetime
 import time
-#channel = 3
+channel = 3
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 def my_callback(channel):
     if GPIO.input(channel) == GPIO.HIGH:
