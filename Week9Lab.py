25fe94b1ede39f58457d0889d6a71382905c6039
Scribe101 <123216616+Scribe101@users.noreply.github.com>
2023-03-17 15:26:07 -0700

    Update Week9Lab.py

--- Week9Lab.py
+++ Week9Lab.py
@@ -10,13 +10,16 @@ def my_callback(channel):
     if GPIO.input(channel) == GPIO.HIGH:
         print('\n▼  at ' + str(datetime.datetime.now()))
     else:
-        print('\n ▲ at ' + str(datetime.datetime.now())) 
-      
-channel = GPIO.wait_for_edge(channel, GPIO.RISING, timeout=5000)
+        print('\n ▲ at ' + str(datetime.datetime.now()))
+
+
 try:
  GPIO.setmode(GPIO.BCM)
  GPIO.setup(6, GPIO.IN)
  GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback)
+
+channel = GPIO.wait_for_edge(channel, GPIO.RISING, timeout=5000)
+
 
 
 
