9bb4ab07421d298c5ca4c3db0f852dbc98c90cf6
Scribe101 <123216616+Scribe101@users.noreply.github.com>
2023-03-17 15:40:30 -0700

    Update Week9Lab.py

--- Week9Lab.py
+++ Week9Lab.py
@@ -1,36 +1,15 @@
-# coding=utf-8
- 
 import RPi.GPIO as GPIO
 import datetime
 import time
-channel = 3
-#GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+channel = 2
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
 
- 
-
-def my_callback(channel):
-    if GPIO.input(channel) == GPIO.HIGH:
-        print('\n▼  at ' + str(datetime.datetime.now()))
-    else:
-        print('\n ▲ at ' + str(datetime.datetime.now()))
-
-
-
-try:
- GPIO.setmode(GPIO.BCM)
- GPIO.setup(6, GPIO.IN)
- channel = GPIO.wait_for_edge(channel, GPIO.FALLING, timeout=5000)
- GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback)
-
-
-
-
-
-
-finally:
-    GPIO.cleanup()
- 
-#GPIO.cleanup()
-print("Goodbye!")
+i = 0
+while i < 10:
+ GPIO.wait_for_edge(channel, GPIO.FALLING)
+ # timeout
+ print(str(datetime.datetime.now()))
+ i = i + 1
+ time.sleep(0.001)
