3d0ea2cdd51efbd2515ec9ca71e077f62e529b3c
Scribe101 <123216616+Scribe101@users.noreply.github.com>
2023-03-24 15:06:48 -0700

    Update Week9Lab.py

--- Week9Lab.py
+++ Week9Lab.py
@@ -41,7 +41,7 @@ while iTime < (originalStart + int(run_time)):
  iTime = int(time.time())
  if iTime > (startTime + int(count_per_entry)):
   end_point = str(datetime.datetime.now())
-  str1 = start_point + end_point
+  str1 = start_point + "<> to <>" + end_point
   data = [str(count),str1]
   writer.writerow(data)
   print("Counts in the last minute: " + str(count))
